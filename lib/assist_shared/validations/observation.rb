abcd44923f9241ac9f259e24147af2d5005e32b5
Scott Macfarlane <macfarlane.scott@gmail.com>
2012-09-19 18:09:21 -0800

    Add finalize attribute to observation
    Also override save.  This allows disabling of validations even in gems
    that call save without being able to pass options.

--- lib/assist_shared/validations/observation.rb
+++ lib/assist_shared/validations/observation.rb
@@ -2,10 +2,11 @@ module AssistShared
   module Validations
     module Observation
       extend ActiveSupport::Concern
-      include ActiveModel::Validations 
       include ActiveModel::Validations
       
       included do
+        attr_accessor :finalize
+
         validates_presence_of :primary_observer, :obs_datetime, :latitude, :longitude, :hexcode
         validates_uniqueness_of :hexcode
         
@@ -16,6 +17,20 @@ module AssistShared
           true
         end
       end
+      
+      def finalize?
+        self.finalize.nil? ? true : self.finalize
+      end
+     
+      def finalize!
+        self.finalize = true
+      end
+    
+      def save opts={}
+        opts.merge!(validate: self.finalize?)
+        super opts
+      end
+    
     end
   end
 end
