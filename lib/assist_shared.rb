77a061e326e3f16712f16c705085dd5e5ee8033f
Scott Macfarlane <macfarlane.scott@gmail.com>
2012-10-10 10:24:31 -0800

    Remove unused files

--- lib/assist_shared.rb
+++ lib/assist_shared.rb
@@ -2,6 +2,5 @@
 $: << File.expand_path('../lib', File.dirname(__FILE__))
 
 require 'assist_shared/version'
-require 'assist_shared/export'
 require 'assist_shared/validations'
 require 'assist_shared/csv'
