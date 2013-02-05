3ef6b873bce262b3639d2c092068d47610f65219
Scott Macfarlane <macfarlane.scott@gmail.com>
2013-02-05 13:02:18 -0900

    Fix misplaed )

--- lib/assist_shared/validations/observation.rb
+++ lib/assist_shared/validations/observation.rb
@@ -20,7 +20,7 @@ module AssistShared
         
         def partial_concentrations_equal_total_concentration
           unless total_concentration == ice_observations.inject(0){|sum, p| sum + p.partial_concentration.to_i}
-            errors.add(:base), "Partial concentrations must equal total concentration"
+            errors.add(:base, "Partial concentrations must equal total concentration")
           end
         end
       end
