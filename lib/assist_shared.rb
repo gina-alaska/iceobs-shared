70cd7dd2d1538adbc696d5ffc125dd44a156c9e3
Scott Macfarlane <scott@gina.alaska.edu>
2014-06-04 12:15:37 -0800

    Actually include the files

--- lib/assist_shared.rb
+++ lib/assist_shared.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 $: << File.expand_path('../lib', File.dirname(__FILE__))
 
+require 'assist_shared/concerns'
 require 'assist_shared/version'
 require 'assist_shared/validations'
 require 'assist_shared/csv'
---
+++ lib/assist_shared/concerns.rb
@@ -0,0 +1,5 @@
+module AssistShared
+  module Concerns
+    AssistShared::Concerns.autoload :IceType, 'assist_shared/concerns/ice_type'
+  end
+end
